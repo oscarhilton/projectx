2ae7fc855fc0116f96c1dc3840798c3b6e132fcd
Oscar Hilton <oscar.hilton@hyd.agency>
2021-02-21 16:21:46 +0000

    new build

--- tsbuild/index.js
+++ tsbuild/index.js
@@ -5,7 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var Stage_1 = __importDefault(require("./Stage"));
 var GAFactory_1 = __importDefault(require("./GAFactory"));
-var stage = new Stage_1.default("testStage");
+var stage = new Stage_1.default("mainStage");
 var title = new GAFactory_1.default(300, "Artemis Harp", 0.01);
 var subTitle = new GAFactory_1.default(150, "Defining greatness in deep tech leadership", 0.02);
 function loopFrame() {
@@ -14,7 +14,6 @@ function loopFrame() {
     stage.clear();
     stage.drawTitle(title.getResultsArray());
     stage.drawSubtitle(subTitle.getResultsArray());
-    console.log(subTitle);
     window.requestAnimationFrame(loopFrame);
 }
 loopFrame();
